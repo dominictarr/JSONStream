e787e940cec4f32c3ae3c8a09f23fbbad0fa4924
James Halliday <mail@substack.net>
2012-02-22 07:52:57 -0800

    failing test for unimplemented stringifyObject

---
+++ test/stringify_object.js
@@ -0,0 +1,42 @@
+
+var fs = require ('fs')
+  , join = require('path').join
+  , file = join(__dirname, 'fixtures','all_npm.json')
+  , JSONStream = require('../')
+  , it = require('it-is').style('colour')
+
+  function randomObj () {
+    return (
+      Math.random () < 0.4
+      ? {hello: 'eonuhckmqjk',
+          whatever: 236515,
+          lies: true,
+          nothing: [null],
+          stuff: [Math.random(),Math.random(),Math.random()]
+        } 
+      : ['AOREC', 'reoubaor', {ouec: 62642}, [[[], {}, 53]]]
+    )
+  }
+
+var expected =  {}
+  , stringify = JSONStream.stringifyObject()
+  , es = require('event-stream')
+  , stringified = ''
+  , called = 0
+  , count = 10
+  , ended = false
+  
+while (count --)
+  var key = Math.random().toString(16).slice(2)
+  expected[key] = randomObj()
+  stringify.write(key, randomObj)
+
+  es.connect(
+    stringify,
+    //JSONStream.parse([/./]),
+    es.writeArray(function (err, lines) {
+      
+      it(JSON.parse(lines.join(''))).deepEqual(expected)
+      console.error('PASSED')
+    })
+  )
