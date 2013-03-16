4283c01e79e7491b6f0cf48c0568a6ad5ae7e7aa
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-16 13:34:50 +1300

    make work with 10

--- test/destroy_missing.js
+++ test/destroy_missing.js
@@ -7,16 +7,21 @@ var JSONStream = require('../');
 
 var server = net.createServer(function(client) {
     var parser = JSONStream.parse([]);
-    parser.on('close', function() {
+    parser.on('end', function() {
+        console.log('close')
         console.error('PASSED');
         server.close();
     });
     client.pipe(parser);
-    client.destroy();
+    var n = 4
+    client.on('data', function () {
+      if(--n) return
+      client.end();
+    })
 });
 server.listen(9999);
 
 
 var client = net.connect({ port : 9999 }, function() {
-    fs.createReadStream(file).pipe(client);
+    fs.createReadStream(file).pipe(client).on('data', console.log) //.resume();
 });
