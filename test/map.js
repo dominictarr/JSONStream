9d9aa891ec8d952e8ffe08cbc8453650f59335b1
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-13 15:48:12 +0200

    test using map function as filter

--- test/map.js
+++ test/map.js
@@ -19,3 +19,22 @@ test('map function', function (t) {
   stream.end()
 
 })
+
+test('filter function', function (t) {
+
+  var actual = []
+
+  stream = JSONStream
+    .parse([true], function (e) { return e%2 ? e : null})
+    .on('data', function (v) { actual.push(v)})
+    .on('end', function () {
+      t.deepEqual(actual, [1,3,5])
+      t.end()
+
+    })
+
+  stream.write(JSON.stringify([1,2,3,4,5,6], null, 2))
+  stream.end()
+
+})
+
