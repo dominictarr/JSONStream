b698048aa4f263e969b79683e6853c5b516ce4aa
Bartosz Sosnowski <bartosz@janeasystems.com>
2018-07-09 19:02:18 +0200

    fixup: older js style

--- test/run.js
+++ test/run.js
@@ -1,13 +1,13 @@
-const { readdirSync } = require('fs')
-const { spawnSync } = require('child_process')
-const { extname } = require('path')
+var readdirSync = require('fs').readdirSync
+var spawnSync = require('child_process').spawnSync
+var extname = require('path').extname
 
-const files = readdirSync(__dirname)
-files.forEach((file) => {
+var files = readdirSync(__dirname)
+files.forEach(function(file){
   if (extname(file) !== '.js' || file === 'run.js')
     return
   console.log(`*** ${file} ***`)
-  const result = spawnSync(process.argv0, [file], { stdio: 'inherit', cwd: __dirname} )
+  var result = spawnSync(process.argv0, [file], { stdio: 'inherit', cwd: __dirname} )
   if (result.status !== 0)
     process.exit(result.status)
 })
